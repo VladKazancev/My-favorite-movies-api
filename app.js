872792412965c43faaeddcca65cd6a1c77eb14b6
Vlad Kazancev <Kazanc3v.V@yandex.ru>
2020-12-16 09:55:50 +0300

    Delete view engine, delete routing

--- app.js
+++ app.js
@@ -8,25 +8,16 @@ require("reflect-metadata");
 require("dotenv").config();
 const { createConnection } = require("typeorm");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
 const app = express();
 
 createConnection();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
